d8c57bec22603763fa1f27d9baa2b7f5c97dedc3
Chris <zhanggopher895@gmail.com>
2026-03-29 14:29:37 -0400

    fix: removed the duplicate types

--- lib/types.ts
+++ lib/types.ts
@@ -1,38 +1,38 @@
 export interface Image {
-  url: string
-  imgix_url?: string
+  url: string;
+  imgix_url?: string;
 }
 
 export interface ArticleMetadata {
-  writer: string
-  review_contributors: string
-  editor_s: string
-  managing_editor?: string
-  publish_date?: string
-  tagline: string
-  cover_image: Image
-  body_content: string
-  in_paragraph_video?: Image
+  writer: string;
+  review_contributors: string;
+  editor_s: string;
+  managing_editor?: string;
+  publish_date?: string;
+  tagline: string;
+  cover_image: Image;
+  body_content: string;
+  in_paragraph_video?: Image;
 }
 
 export interface Article {
-  id: string
-  slug: string
-  title: string
-  type: "article"
-  created_at?: string
-  modified_at?: string
-  published_at?: string
-  thumbnail?: string
-  metadata: ArticleMetadata
+  id: string;
+  slug: string;
+  title: string;
+  type: "article";
+  created_at?: string;
+  modified_at?: string;
+  published_at?: string;
+  thumbnail?: string;
+  metadata: ArticleMetadata;
 }
 
 export interface CosmicResponse<T> {
-  object: T | null
+  object: T | null;
 }
 
 export interface CosmicListResponse<T> {
-  objects: T[]
-  total: number
-  limit?: number
+  objects: T[];
+  total: number;
+  limit?: number;
 }
