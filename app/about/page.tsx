93b3a5eed0c7bfb91efe436c2625f36e040cf046
annikabaking <annika_baking@brown.edu>
2026-04-05 00:38:29 -0400

    added eboard pic (about page)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
+import eboardPic1 from '../assets/eboardPic1.jpg';
+
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center">
+        <div className="flex flex-col min-h-screen items-center pt-24 pb-24">
             <span className="text-xs font-mono tracking-widest uppercase text-red-600 mb-2">
                 About Us
             </span>
@@ -18,7 +20,35 @@ export default function Home() {
             <span className="text-xs font-mono tracking-widest uppercase text-red-600 mb-2 mt-16">
                 Meet our staff
             </span>
-            <h2 className="text-4xl font-bold font-serif mb-2">Eboard? Writers?</h2>
+            <h2 className="text-4xl font-bold font-serif mb-2">E-Board</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-5xl mt-10">
+
+                <div className="flex flex-col">
+                    <div className="w-full mb-4">
+                        <img src={eboardPic1.src} className="w-full aspect-square object-cover" />
+                    </div>
+                    <h3 className="text-gray-500 font-medium mb-1 tracking-wide">Thomas</h3>
+                    <p className="text-gray-400 text-sm">bio</p>
+                </div>
+
+                <div className="flex flex-col">
+                    <div className="w-full mb-4">
+                        <img src={eboardPic1.src} className="w-full aspect-square object-cover" />
+                    </div>
+                    <h3 className="text-gray-500 font-medium mb-1 tracking-wide">Thomas</h3>
+                    <p className="text-gray-400 text-sm">bio</p>
+                </div>
+
+                <div className="flex flex-col">
+                    <div className="w-full mb-4">
+                        <img src={eboardPic1.src} className="w-full aspect-square object-cover" />
+                    </div>
+                    <h3 className="text-gray-500 font-medium mb-1 tracking-wide">Thomas</h3>
+                    <p className="text-gray-400 text-sm">bio</p>
+                </div>
+
+            </div>
         </div>
     );
 }
